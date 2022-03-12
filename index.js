11ef00422fa9dd6d81fb93787582acfd372a06a6
Ratul <ratuldawonauth@gmail.com>
2022-03-11 19:18:09 -0800

    Fixed the Template Literals

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
+require('dotenv').config();
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
-  console.log('Logged in as ${client.user.tag}!')
+  console.log(`Logged in as ${client.user.tag}!`)
 });
 
-/**
- * const Discord = require("discord.js");
- * const client = new Discord.Client();
- * 
- * client.on("ready", () => {
- *   console.log('Logged in as ${client.user.tag}!')
- * })
- */
+client.login(process.env.TOKEN);
