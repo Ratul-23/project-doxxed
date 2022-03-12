3dd7564654140d75b487ebfbc00c5f54982b5a5a
Ratul <ratuldawonauth@gmail.com>
2022-03-11 20:44:09 -0800

    Several Clean-Ups

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const { Client, Intents } = require('discord.js');
 // Environmental Variable Library
 require('dotenv').config();
 
-
+// Creating the main hub for interaction with the Discord API
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.DISCORD_TOKEN);
